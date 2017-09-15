dcb81e1d2617ec99f0e26eb2f24c8f2a375f1e0c
Koukuntla, Sanjana (Navya) <Sanjana.Koukuntla@contractor.cengage.com>
2017-09-15 15:16:49 -0400

    changes to translate it works in office only because of dev

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('detectApp', ['ngMaterial']);
+var app = angular.module('detectApp', ['ngMaterial','ngRoute']);
     
     app.controller('transContrl',[ '$http','$scope', function ($http,$scope){
         $scope.transarray = [{Query:'microscopia'},{Query:'odontologia'},{Query:'teologia'},{Query:'mujeres gestantes'},{Query:'hija'},{Query:'date de naissance'},{Query:'contabilidade gerencial'}];
@@ -14,16 +14,65 @@ var app = angular.module('detectApp', ['ngMaterial']);
         }
         $scope.search = function(trans){
             
-            $http({url:'http://api.galegroup.com/api/v1/items?collection.id=AONE&api_key=0e473e40-8db0-11e7-9d14-005056b845b8&q=war',method: 'GET',headers: {
+            $http({url:'http://api-as01.dev.gale.web:8080/api/v1/items?q=happy&api_key=api-1234',method: 'GET',headers: {
                     'Content-Type': 'application/json; charset=utf-8'
         }}).then(function success(result){
-                    $scope.count = result;
+                    $scope.result = result.data.docs;
+                    $scope.count.response = $scope.response;
             });
         }
         
     }]);
 
-    app.config(function ($httpProvider) {
+    app.controller('translateContrl',[ '$http','$scope', function ($http,$scope){
+        $scope.target='en';
+        $scope.show = false;
+        $scope.translatesearch = function(){
+            $http.get('https://www.googleapis.com/language/translate/v2?key=AIzaSyCSkJzCc7-jPArWHMYCeWSaIstDTzO7iYY&target='+$scope.target+'&q='+$scope.word).then(function(response){
+                debugger;
+                $scope.response = response.data.data.translations;
+                var word = $scope.word;
+                var translatedword = $scope.response[0].translatedText;
+                $scope.targettitle = $scope.response[0].detectedSourceLanguage;
+                $scope.search(translatedword);
+            })
+        }
+        $scope.translate = function(){
+            $http.get('https://www.googleapis.com/language/translate/v2?key=AIzaSyCSkJzCc7-jPArWHMYCeWSaIstDTzO7iYY&target='+$scope.target+'&q='+$scope.word).then(function(response){
+                debugger;
+                $scope.response = response.data.data.translations;
+                var word = $scope.word;
+                $scope.search(word);
+            })
+        }
+        $scope.translateTitle = function(r){
+            $http.get('https://www.googleapis.com/language/translate/v2?key=AIzaSyCSkJzCc7-jPArWHMYCeWSaIstDTzO7iYY&target='+$scope.targettitle+'&q='+r.title).then(function(response){
+                debugger;
+                $scope.show = true;
+                $scope.translatedTitle = response.data.data.translations;
+                r.translatedTitle = $scope.translatedTitle[0].translatedText;
+            })
+        }
+
+        $scope.search = function(word){
+            $http({url:'http://api-as01.dev.gale.web:8080/api/v1/items?callback=JSON_CALLBACK&q='+word+'&api_key=api-1234',method: 'JSONP'}).then(function success(result){
+                $scope.countresults = result.data.count;    
+                $scope.result = result.data.docs;
+            });
+        }
         
-});
+    }]);
+
+    app.config(function ($httpProvider, $routeProvider) {
+        $routeProvider
+        .when("/translation", {
+            templateUrl : "/resources/templates/translate.html",
+            controller : "translateContrl"
+        })
+        .when("/first", {
+            templateUrl : "/resources/templates/first.html",
+            controller : "transContrl"
+        })
+        
+    });
 
