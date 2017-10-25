1b1a2f77ba9d9507b8bdc7f46baae16e46eddf68
Sanjana17 <navyasanjana4@gmail.com>
2017-10-25 15:42:09 -0400

    removed comments

--- js/app.js
+++ js/app.js
@@ -1,28 +1,5 @@
 var app = angular.module('detectApp', ['ngMaterial','ngRoute']);
 
-// app.controller('transContrl',[ '$http','$scope', function ($http,$scope){
-//     $scope.transarray = [{Query:'microscopia'},{Query:'odontologia'},{Query:'teologia'},{Query:'mujeres gestantes'},{Query:'hija'},{Query:'date de naissance'},{Query:'contabilidade gerencial'}];
-//     $scope.target='en';
-//     $scope.show = false;
-//     $scope.translate = function(trans){
-//         $http.get('https://www.googleapis.com/language/translate/v2?key=AIzaSyCSkJzCc7-jPArWHMYCeWSaIstDTzO7iYY&target='+$scope.target+'&q='+trans.Query).then(function(response){
-//             debugger;
-//             $scope.response = response.data.data.translations;
-//             $scope.search(trans);
-//             $scope.index=$scope.transarray.indexOf(trans)
-//         })
-//     }
-//     $scope.search = function(trans){
-//         $http({url:'http://api-as01.dev.gale.web:8080/api/v1/items?q=happy&api_key=api-1234',method: 'GET',headers: {
-//             'Content-Type': 'application/json; charset=utf-8'
-//         }}).then(function success(result){
-//             $scope.result = result.data.docs;
-//             $scope.count.response = $scope.response;
-//         });
-//     }
-//
-// }]);
-
 app.controller('translateContrl',[ '$http','$scope', function ($http,$scope){
     $scope.target='en';
     $scope.show = "translate";
@@ -109,8 +86,6 @@ app.controller('translateContrl',[ '$http','$scope', function ($http,$scope){
     }
 
     $scope.search = function(translang){
-        //var word = $scope.word;
-        //var testUrl='https://newsapi.org/v1/articles?source='+word.split(' ').join('')+'&apiKey=10491e51250442cd96af1b3dbeefe7f1';
         var original='http://api-as01.dev.gale.web:8080/api/v1/items?callback=JSON_CALLBACK&q='+translatedword+'&api_key=api-1234';
         $scope.isLoading=true;
         $http({url:original,method:'JSONP'}).then(function success(result){
@@ -159,10 +134,6 @@ app.config(function ($httpProvider, $routeProvider) {
         .when("/translation", {
             templateUrl : "/resources/templates/translate.html",
             controller : "translateContrl"
-        })
-        .when("/first", {
-            templateUrl : "/resources/templates/first.html",
-            controller : "transContrl"
         })
 
 });
